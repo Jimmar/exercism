c91311ca93904a03e1219b2021f15b8798af76d9
Jimmar <justjimmar@gmail.com>
2023-07-25 19:02:35 +0300

    finally

--- elixir/dna-encoding/lib/dna.ex
+++ elixir/dna-encoding/lib/dna.ex
@@ -1,14 +1,50 @@
+# defmodule DNA do
+#   @nucleotide %{?\s => 0b0000, ?A => 0b0001, ?C => 0b0010, ?G => 0b0100, ?T => 0b1000}
+#   @nucleotide_rev Map.new(@nucleotide, fn {key, val} -> {val, key} end)
+
+#   def encode_nucleotide(code_point), do: @nucleotide[code_point]
+
+#   def decode_nucleotide(encoded_code), do: @nucleotide_rev[encoded_code]
+
+#   def encode([]), do: <<>>
+#   def encode([head | tail]), do: <<encode_nucleotide(head)::4, encode(tail)::bitstring>>
+
+#   def decode(<<>>), do: []
+#   def decode(<<head::4, tail::bitstring>>), do: [decode_nucleotide(head) | decode(tail)]
+# end
+
 defmodule DNA do
-  @nucleotide %{?\s => 0b0000, ?A => 0b0001, ?C => 0b0010, ?G => 0b0100, ?T => 0b1000}
-  @nucleotide_rev Map.new(@nucleotide, fn {key, val} -> {val, key} end)
+  @nucleotide_encoder %{
+    ?\s => 0b0000,
+    ?A => 0b0001,
+    ?C => 0b0010,
+    ?G => 0b0100,
+    ?T => 0b1000
+  }
 
-  def encode_nucleotide(code_point), do: @nucleotide[code_point]
+  # @nucleotide_decoder Map.new(@nucleotide_encoder, fn {key, val} -> {val, key} end)
+  @nucleotide_decoder Map.new(@nucleotide_encoder, fn {k, v} -> {v, k} end)
 
-  def decode_nucleotide(encoded_code), do: @nucleotide_rev[encoded_code]
+  def encode_nucleotide(code_point) do
+    Map.fetch!(@nucleotide_encoder, code_point)
+  end
 
+  def decode_nucleotide(encoded_code) do
+    # @nucleotide_decoder[encoded_code]
+    Map.fetch!(@nucleotide_decoder, encoded_code)
+  end
+
+  def encode(dna)
   def encode([]), do: <<>>
-  def encode([head | tail]), do: <<encode_nucleotide(head)::4, encode(tail)::bitstring>>
 
+  def encode([head | tail]) do
+    <<encode_nucleotide(head)::4, encode(tail)::bitstring>>
+  end
+
+  def decode(dna)
   def decode(<<>>), do: []
-  def decode(<<head::4, tail::bitstring>>), do: [decode_nucleotide(head) | decode(tail)]
+
+  def decode(<<head::4, tail::bitstring>>) do
+    [decode_nucleotide(head) | decode(tail)]
+  end
 end
--- elixir/library-fees/lib/library_fees.ex
+++ elixir/library-fees/lib/library_fees.ex
@@ -1,24 +1,69 @@
-defmodule LibraryFees do
-  def datetime_from_string(string), do: NaiveDateTime.from_iso8601!(string)
+# defmodule LibraryFees do
+#   def datetime_from_string(string), do: NaiveDateTime.from_iso8601!(string)
 
-  def before_noon?(datetime), do: datetime.hour < 12
+#   def before_noon?(datetime), do: datetime.hour < 12
+
+#   def return_date(checkout_datetime) do
+#     checkout_datetime
+#     |> NaiveDateTime.to_date()
+#     |> Date.add(if before_noon?(checkout_datetime), do: 28, else: 29)
+#   end
+
+#   def days_late(planned_return_date, actual_return_datetime) do
+#     Date.diff(actual_return_datetime, planned_return_date) |> max(0)
+#   end
+
+#   def monday?(datetime), do: datetime |> Date.day_of_week() == 1
+
+#   def calculate_late_fee(checkout, return, rate) do
+#     return_datetime = return |> datetime_from_string()
+#     checkout_datetime = checkout |> datetime_from_string()
+#     fees = checkout_datetime |> return_date |> days_late(return_datetime) |> Kernel.*(rate)
+#     if monday?(return_datetime), do: (fees * 0.5) |> floor(), else: fees
+#   end
+# end
+
+
+defmodule LibraryFees do
+  def datetime_from_string(string) do
+    NaiveDateTime.from_iso8601!(string)
+  end
+  def before_noon?(datetime) do
+    datetime.hour < 12
+  end
 
   def return_date(checkout_datetime) do
-    checkout_datetime
-    |> NaiveDateTime.to_date()
-    |> Date.add(if before_noon?(checkout_datetime), do: 28, else: 29)
+    # checkout_date = Date.new!(:year, :month, :day)
+    checkout_date = checkout_datetime |> NaiveDateTime.to_date()
+
+    if before_noon?(checkout_datetime) do
+      Date.add(checkout_date, 28)
+    else
+      Date.add(checkout_date, 29)
+    end
   end
 
   def days_late(planned_return_date, actual_return_datetime) do
-    Date.diff(actual_return_datetime, planned_return_date) |> max(0)
+    actual_return_datetime
+    |> Date.diff(planned_return_date)
+    |> max(0)
   end
 
-  def monday?(datetime), do: datetime |> Date.day_of_week() == 1
+  def monday?(datetime) do
+    Date.day_of_week(datetime) == 1
+  end
 
   def calculate_late_fee(checkout, return, rate) do
-    return_datetime = return |> datetime_from_string()
-    checkout_datetime = checkout |> datetime_from_string()
-    fees = checkout_datetime |> return_date |> days_late(return_datetime) |> Kernel.*(rate)
-    if monday?(return_datetime), do: (fees * 0.5) |> floor(), else: fees
+    checkout_datetime = datetime_from_string(checkout)
+    return_datetime = datetime_from_string(return)
+
+    late_fee = days_late(return_date(checkout_datetime), return_datetime ) * rate
+    # late_fee = checkout_datetime |> return_date |> days_late(return_datetime) |> Kernel.*(rate)
+
+    if monday?(return_datetime) do
+      floor(late_fee * 0.5)
+    else
+      late_fee
+    end
   end
 end
